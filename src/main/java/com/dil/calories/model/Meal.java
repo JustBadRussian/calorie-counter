417b43ff283115545a11b239391c43898905f29a
Andrey Dil <siberia.novosibirsk.dil@gmail.com>
2019-08-05 00:20:01 +0700

    Adding classes Meal and User into package model

---
+++ src/main/java/com/dil/calories/model/Meal.java
@@ -0,0 +1,52 @@
+package com.dil.calories.model;
+
+import java.time.LocalDateTime;
+
+public class Meal {
+    private Integer id;
+
+    private LocalDateTime dateTime;
+
+    private String description;
+
+    private int calories;
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+}
